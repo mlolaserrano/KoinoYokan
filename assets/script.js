ad40536e52f137f1a78e6255e0f23a9c8aa3bd45
Lola <31747974+mlolaserrano@users.noreply.github.com>
2024-11-08 15:48:28 -0300

    tienda

--- assets/script.js
+++ assets/script.js
@@ -192,42 +192,6 @@ class componenteFooter extends HTMLElement {
 }
 customElements.define('componente-footer', componenteFooter);
 
-// JSON: Cards con productos
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('/productos.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const productos = data.productos;
-            const productosContainer = document.getElementById("productos-container");
-
-            if (productosContainer) {
-                productos.forEach(producto => {
-                    if (producto.stock > 0) {
-                        const card = document.createElement("div");
-                        card.classList.add("custom-card");
-
-                        card.innerHTML = `  
-                            <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
-                            <div class="card-body">  
-                                <h5 class="card-title" id="tiendatexto">${producto.nombre}</h5>  
-                                <p class="card-text" id="tiendatexto">${producto.descripcion}</p>  
-                                <p class="card-text" id="tiendatexto">Precio: AR$${producto.precio}</p>  
-                                <p class="card-text" id="tiendatexto">Stock: ${producto.stock}</p>  
-                            </div>  
-                        `;
-
-                        productosContainer.appendChild(card);
-                    }
-                });
-            }
-        })
-        .catch(error => console.error('Error al cargar el JSON:', error));
-});
 
 
 // JSON: Login 
